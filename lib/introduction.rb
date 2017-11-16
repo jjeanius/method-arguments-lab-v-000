39274d72f2ffde4576a3e4edc7183eddfc2c48c5
Judy McGoldrick <jjeanius@verizon.net>
2017-11-16 16:27:34 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,8 +3,6 @@ def introduction (name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language ( name, language )
-  name = "Dan"
-  language = "Ember.js"
+def introduction_with_language ("Dan", "Ember.js")
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
